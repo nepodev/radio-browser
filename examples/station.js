92ebee6f3a7c6949b0161a9991cec393f09357f6
nepodev <nepodev@gmx.com>
2021-04-17 07:51:44 +0200

    example output with uuid

--- examples/station.js
+++ examples/station.js
@@ -2,5 +2,5 @@
 
 module.exports = function(station)
 {
-    console.log(`${station.name}\nStream: ${station.url}\nTags : ${station.tags}\nVotes: ${station.votes}\nClicks: ${station.clickcount}\n`)
+    console.log(`${station.name}\nStation UUID: ${station.stationuuid}\nStream: ${station.url}\nTags : ${station.tags}\nVotes: ${station.votes}\nClicks: ${station.clickcount}\n`)
 }
